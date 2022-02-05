bc356d5e697d44afc61b5f5b2a3566023779a3cc
Santiago <sntg98@hotmail.com>
2022-02-05 12:27:17 -0500

    modificacion en el script de reclamos

--- entidades/reclamos.sql
+++ entidades/reclamos.sql
@@ -1,23 +1,23 @@
--- Table: public.RadicadosPQ
+-- Table: public.reclamos
 
--- DROP TABLE IF EXISTS public."RadicadosPQ";
+-- DROP TABLE IF EXISTS public.reclamos;
 
-CREATE TABLE IF NOT EXISTS public."RadicadosPQ"
+CREATE TABLE IF NOT EXISTS public.reclamos
 (
-    id_radicado bigint NOT NULL DEFAULT nextval('"RadicadosPQ_id_radicado_seq"'::regclass),
-    id_usuario bigint NOT NULL,
-    tipo character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    id_reclamo bigint NOT NULL DEFAULT nextval('"Reclamo_id_reclamo_seq"'::regclass),
+    id_radicado bigint NOT NULL,
+    reclamo character varying(255) COLLATE pg_catalog."default" NOT NULL,
     fecha_creacion date NOT NULL,
-    fecha_modificacion date NOT NULL,
-    contenido character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    CONSTRAINT "RadicadosPQ_pkey" PRIMARY KEY (id_radicado),
-    CONSTRAINT "usuario_radicadoPQ" FOREIGN KEY (id_usuario)
-        REFERENCES public."Usuarios" (id_usuario) MATCH SIMPLE
+    CONSTRAINT "Reclamo_pkey" PRIMARY KEY (id_reclamo),
+    CONSTRAINT reclamos_id_radicado_key UNIQUE (id_radicado),
+    CONSTRAINT radicado_reclamo FOREIGN KEY (id_radicado)
+        REFERENCES public.radicadospq (id_radicado) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
+        NOT VALID
 )
 
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS public."RadicadosPQ"
+ALTER TABLE IF EXISTS public.reclamos
     OWNER to postgres;
